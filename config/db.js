657721b3fc25e00dc78a72b1769a9ca8094907f3
ParamMavani <parammavani16@gmail.com>
2026-04-18 21:59:42 +0530

    updated db

--- config/db.js
+++ config/db.js
@@ -1,15 +1,19 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'bookmart',
-  port: process.env.DB_PORT || 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+
+  ssl: {
+    rejectUnauthorized: false
+  },
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
-});
+}); 
 
-// The .promise() is CRITICAL here so we can use await db.query() in our routes
 module.exports = pool.promise();
